848a65508c1f4f32d5e912c1317b480336b532d6
TurtleWolfe <JonPohlner@gmail.com>
2021-01-12 23:17:18 -0500

    Started Demo for SaStudio

--- src/components/SaStudio/SaStudio.js
+++ src/components/SaStudio/SaStudio.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import { Container, Row, Image } from 'react-bootstrap';
-import fccCert from '../../images/fccCertificate.png';
+import saStudiooo from './images/SaStudio.png';
+import saStudioo1 from './images/SaStudio(1).png';
 import './SaStudio.css';
 
 
 export const About = () => {
   return (
-    <Container className="About justify-content-center">
-      {/* <ul>
-          <li>
-            <a href="https://codepen.io/TurtleWolf/">
-              <i className="fa fa-codepen" aria-hidden="true" /> CodePen
-                </a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/groups/266866570176327/">
-              <i className="fa fa-facebook-official" aria-hidden="true" />{" "}
-                  DangerousMinds
-                </a>
-          </li>
-          <li>
-            <a href="https://www.freecodecamp.org/turtlewolf">
-              <i className="fa fa-free-code-camp" aria-hidden="true" />{" "}
-                  FreeCodeCamp
-                </a>
-          </li>
-        </ul> */}
+    <Container className="saStudioo justify-content-center">
 
       <Row as="a" // GitHub
         className="ispace App-link justify-content-center"
@@ -37,19 +19,19 @@ export const About = () => {
         <i className="ispace fab fa-github"></i> GitHub <i className="ispace fab fa-github"></i>
       </Row>
 
-      <strong>  {/* Twitch */}
+      <strong>  {/* https://sastudio.org/ */}
         <Row as="a"
           className="ispace App-link justify-content-center"
-          href="https://www.twitch.tv/turtlewolfe"
+          href="https://sastudio.org/"
           target="_blank"
           rel="noopener noreferrer"
-          title="Learn React"
+          title="Rent a private room in this beautiful 100+ year old church that is now a community of artists, educators, healing ministries and locally sourced grocery store."
         >
-          <i class="ispace fab fa-twitch"></i> Twitch <i class="ispace fab fa-twitch"></i>
+          <i class="ispace fab fa-twitch"></i> https://sastudio.org/ <i class="ispace fab fa-twitch"></i>
         </Row>
       </strong>
 
-      <Row as="a" // Twitter
+      {/* <Row as="a" // Twitter
         className="ispace App-link justify-content-center"
         // href="https://twitter.com/JonPohlner?lang=en"
         href="https://twitter.com/ScriptHammer?lang=en"
@@ -58,9 +40,9 @@ export const About = () => {
         title="Learn React"
       >
         <i class="ispace fab fa-twitter"></i> Twitter <i class="ispace fab fa-twitter"></i>
-      </Row>
+      </Row> */}
 
-      <Row as="a" // YouTube
+      {/* <Row as="a" // YouTube
         className="ispace App-link justify-content-center"
         href="https://www.youtube.com/playlist?list=PLETG2T1KvnioI_uRwEx1-efEI6EpoN_ZR"
         target="_blank"
@@ -68,9 +50,9 @@ export const About = () => {
         title="YouTube/TW"
       >
         <i className="ispace fab fa-youtube"></i> YouTube <i className="ispace fab fa-youtube"></i>
-      </Row>
+      </Row> */}
 
-      <Row as="a" // LinkedIn
+      {/* <Row as="a" // LinkedIn
         className="ispace App-link justify-content-center"
         href="https://www.linkedin.com/in/jonathan-pohlner-87796377/"
         target="_blank"
@@ -78,41 +60,14 @@ export const About = () => {
         title="Learn React"
       >
         <i className="ispace fab fa-linkedin"></i> LinkedIn <i className="ispace fab fa-linkedin"></i>
-      </Row>
-
-      {/* <Row as="a"
-        className="ispace App-link justify-content-center"
-        href="https://fontawesome.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Awesome Fonts"
-      >
-        <i className="ispace fab fa-font-awesome-alt"></i> Font Awesome <i className="ispace fab fa-font-awesome-alt"></i>
-      </Row>
-
-      <Row as="a"
-        className="ispace App-link justify-content-center"
-        href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Markdown Cheatsheet"
-      >
-        <i className="ispace fab fa-markdown"></i> Markdown Cheatsheet <i className="ispace fab fa-markdown"></i>
-      </Row>
-
-      <Row as="a"
-        className="ispace App-link justify-content-center"
-        href="https://css-tricks.com/essential-meta-tags-social-media/"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="The Essential Meta Tags for Social Media"
-      >
-        <i className="ispace far fa-file-code"></i> Essential Meta Tags<i className="ispace far fa-file-code"></i>
       </Row> */}
 
       <br></br>
       <Row className="justify-content-center">
-        <Image src={fccCert} fluid />
+        <Image src={saStudiooo} fluid />
+      </Row>
+      <Row className="justify-content-center">
+        <Image src={saStudioo1} fluid />
       </Row>
     </Container>
   );
